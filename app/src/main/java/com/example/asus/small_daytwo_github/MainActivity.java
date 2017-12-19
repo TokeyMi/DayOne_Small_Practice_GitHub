7b912860c1e9f98a94115b8781008a2317446b71
YokeyMi <2082870793@qq.com>
2017-12-19 13:51:40 +0800

    初次提交代码

---
+++ app/src/main/java/com/example/asus/small_daytwo_github/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.asus.small_daytwo_github;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("xxoo");
+
+    }
+}
